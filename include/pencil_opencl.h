6a245ea09c7d988a74d721a984b7401b22e10f7f
Robert David <robert.david@realeyesit.com>
2014-10-14 11:08:42 +0200

    Update headers to a generated version
    Note: the generator program will replace the headers in the near future.

--- include/pencil_opencl.h
+++ include/pencil_opencl.h
@@ -21,27 +21,30 @@
  * 
  * Note: this is a generated file. Do not modify.
  */
-#ifndef PENCIL_LIB_H
-#define PENCIL_LIB_H
-#include <math.h>
-inline char bmin(char a, char b) { return a <= b ? a : b; }
-inline short smin(short a, short b) { return a <= b ? a : b; }
-inline int min(int a, int b) { return a <= b ? a : b; }
-inline long lmin(long a, long b) { return a <= b ? a : b; }
-inline unsigned char ubmin(unsigned char a, unsigned char b) { return a <= b ? a : b; }
-inline unsigned short usmin(unsigned short a, unsigned short b) { return a <= b ? a : b; }
-inline unsigned int umin(unsigned int a, unsigned int b) { return a <= b ? a : b; }
-inline unsigned long ulmin(unsigned long a, unsigned long b) { return a <= b ? a : b; }
-inline char bmax(char a, char b) { return a >= b ? a : b; }
-inline short smax(short a, short b) { return a >= b ? a : b; }
-inline int max(int a, int b) { return a >= b ? a : b; }
-inline long lmax(long a, long b) { return a >= b ? a : b; }
-inline unsigned char ubmax(unsigned char a, unsigned char b) { return a >= b ? a : b; }
-inline unsigned short usmax(unsigned short a, unsigned short b) { return a >= b ? a : b; }
-inline unsigned int umax(unsigned int a, unsigned int b) { return a >= b ? a : b; }
-inline unsigned long ulmax(unsigned long a, unsigned long b) { return a >= b ? a : b; }
-inline float clampf(float val, float min, float max) { return (val < min) ? min : (val > max) ? max : val; }
-inline double clamp(double val, double min, double max) { return (val < min) ? min : (val > max) ? max : val; }
+#ifndef PENCIL_OPENCL_H
+#define PENCIL_OPENCL_H
+#define fminf(a, b) (fmin)((float)(a), (float)(b))
+#define fmin(a, b) (fmin)((double)(a), (double)(b))
+#define bmin(a, b) (min)((char)(a), (char)(b))
+#define smin(a, b) (min)((short)(a), (short)(b))
+#define min(a, b) (min)((int)(a), (int)(b))
+#define lmin(a, b) (min)((long)(a), (long)(b))
+#define ubmin(a, b) (min)((unsigned char)(a), (unsigned char)(b))
+#define usmin(a, b) (min)((unsigned short)(a), (unsigned short)(b))
+#define umin(a, b) (min)((unsigned int)(a), (unsigned int)(b))
+#define ulmin(a, b) (min)((unsigned long)(a), (unsigned long)(b))
+#define fmaxf(a, b) (fmax)((float)(a), (float)(b))
+#define fmax(a, b) (fmax)((double)(a), (double)(b))
+#define bmax(a, b) (max)((char)(a), (char)(b))
+#define smax(a, b) (max)((short)(a), (short)(b))
+#define max(a, b) (max)((int)(a), (int)(b))
+#define lmax(a, b) (max)((long)(a), (long)(b))
+#define ubmax(a, b) (max)((unsigned char)(a), (unsigned char)(b))
+#define usmax(a, b) (max)((unsigned short)(a), (unsigned short)(b))
+#define umax(a, b) (max)((unsigned int)(a), (unsigned int)(b))
+#define ulmax(a, b) (max)((unsigned long)(a), (unsigned long)(b))
+#define clampf(val, min, max) (clamp)((float)(val), (float)(min), (float)(max))
+#define clamp(val, min, max) (clamp)((double)(val), (double)(min), (double)(max))
 inline char bclampi(char val, char min, char max) { return (val < min) ? min : (val > max) ? max : val; }
 inline short sclampi(short val, short min, short max) { return (val < min) ? min : (val > max) ? max : val; }
 inline int clampi(int val, int min, int max) { return (val < min) ? min : (val > max) ? max : val; }
@@ -50,6 +53,20 @@ inline unsigned char ubclampi(unsigned char val, unsigned char min, unsigned cha
 inline unsigned short usclampi(unsigned short val, unsigned short min, unsigned short max) { return (val < min) ? min : (val > max) ? max : val; }
 inline unsigned int uclampi(unsigned int val, unsigned int min, unsigned int max) { return (val < min) ? min : (val > max) ? max : val; }
 inline unsigned long ulclampi(unsigned long val, unsigned long min, unsigned long max) { return (val < min) ? min : (val > max) ? max : val; }
-inline float mixf(float x, float y, float a) { return x + (y-x)*a; }
-inline double mix(double x, double y, double a) { return x + (y-x)*a; }
+#define tanf(x) (tan)((float)(x))
+#define tan(x) (tan)((double)(x))
+#define atanf(x) (atan)((float)(x))
+#define atan(x) (atan)((double)(x))
+#define atan2f(x, y) (atan2)((float)(x), (float)(y))
+#define atan2(x, y) (atan2)((double)(x), (double)(y))
+#define hypotf(x, y) (hypot)((float)(x), (float)(y))
+#define hypot(x, y) (hypot)((double)(x), (double)(y))
+#define expf(x) (exp)((float)(x))
+#define exp(x) (exp)((double)(x))
+#define ceilf(x) (ceil)((float)(x))
+#define ceil(x) (ceil)((double)(x))
+#define floorf(x) (floor)((float)(x))
+#define floor(x) (floor)((double)(x))
+#define mixf(x, y, a) (mix)((float)(x), (float)(y), (float)(a))
+#define mix(x, y, a) (mix)((double)(x), (double)(y), (double)(a))
 #endif
